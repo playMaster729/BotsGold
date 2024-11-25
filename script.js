c37e8ae10a25a7434a8e5159a0953d525b02ccce
playMaster729 <64599204+playMaster729@users.noreply.github.com>
2024-11-25 21:11:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,36 @@
 const SECRET_CODE = "BotsFarmCode=148878";
-let screenHistory = [];
 
 window.onload = function () {
-    const hash = window.location.hash.substring(1);
+    const hash = window.location.hash.substring(1); // Убираем символ #
+    const content = document.getElementById("content");
 
-    if (hash !== SECRET_CODE) {
-        window.location.href = "https://google.com";
+    if (hash === SECRET_CODE) {
+        setTimeout(() => {
+            content.classList.remove("hidden");
+        }, 1000); // Задержка для загрузки интерфейса
+    } else {
+        setTimeout(() => {
+            window.location.href = "https://google.com";
+        }, 1000); // Задержка перед редиректом
     }
 };
 
 function openSection(sectionId) {
-    navigateTo(`${sectionId}-screen`);
+    hideAllScreens();
+    document.getElementById(`${sectionId}-screen`).classList.remove("hidden");
 }
 
 function openSubsection(subsectionId) {
-    navigateTo(`${subsectionId}-screen`);
+    hideAllScreens();
+    document.getElementById(`${subsectionId}-screen`).classList.remove("hidden");
 }
 
 function goBack() {
-    if (screenHistory.length > 1) {
-        // Удаляем текущий экран из истории
-        screenHistory.pop();
-        const previousScreen = screenHistory[screenHistory.length - 1];
-        showScreen(previousScreen);
-    }
+    hideAllScreens();
+    document.getElementById("main-screen").classList.remove("hidden");
 }
 
-function navigateTo(screenId) {
-    // Сохраняем текущий экран в историю
-    if (screenHistory[screenHistory.length - 1] !== screenId) {
-        screenHistory.push(screenId);
-    }
-    showScreen(screenId);
-}
-
-function showScreen(screenId) {
-    // Скрываем все экраны
+function hideAllScreens() {
     const screens = document.querySelectorAll(".container");
     screens.forEach(screen => screen.classList.add("hidden"));
-
-    // Показываем выбранный экран
-    const targetScreen = document.getElementById(screenId);
-    if (targetScreen) {
-        targetScreen.classList.remove("hidden");
-    }
 }
